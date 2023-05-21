b696111514ab770b1952bb43f1702082b5269b2b
tod619 <trevorodwyer619@hotmail.com>
2023-05-21 21:40:19 +0100

    finish card object in cards.py file

--- __pycache__/cards/cards.py
+++ __pycache__/cards/cards.py
@@ -9,3 +9,12 @@ class Card(object):
     RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     # Suits
     SUITS = ["c", "d", "h", "s"]
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    # Return a string representation of a car
+    def __str__(self):
+        rep = self.rank + self.suit
+        return rep
