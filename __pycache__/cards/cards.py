263bdb0d6287edbb35baa9d056ac0c331ca7bd70
tod619 <trevorodwyer619@hotmail.com>
2023-05-21 21:25:41 +0100

    Create Ranks + Suits list in cards.py file

---
+++ __pycache__/cards/cards.py
@@ -0,0 +1,11 @@
+# Playing Cards
+# Demonstrates combining objects
+# 21/05/2023
+
+class Card(object):
+    """A playing card"""
+    # Constants
+    # Card Ranks
+    RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    # Suits
+    SUITS = ["c", "d", "h", "s"]
