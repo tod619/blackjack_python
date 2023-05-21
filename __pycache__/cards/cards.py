e636a7dc1c13c7fda76ebb9b71b39f8cd47c60fd
tod619 <trevorodwyer619@hotmail.com>
2023-05-21 21:51:59 +0100

    create a hand class which is a collection of cards in cards.py

--- __pycache__/cards/cards.py
+++ __pycache__/cards/cards.py
@@ -18,3 +18,31 @@ class Card(object):
     def __str__(self):
         rep = self.rank + self.suit
         return rep
+
+# Create a hand class which is a collection of card objects
+
+
+class Hand(object):
+    """A hand of playing cards"""
+
+    def __init__(self) -> None:
+        self.cards = []
+
+    def __str__(self):
+        if self.cards:
+            rep = ""
+            for card in self.cards:
+                rep += str(card) + " "
+        else:
+            rep = "<empty>"
+        return rep
+
+    def clear(self):
+        self.cards = []
+
+    def add(self, card):
+        self.cards.append(card)
+
+    def give(self, card, other_hand):
+        self.cards.remove(card)
+        other_hand.add(card)
